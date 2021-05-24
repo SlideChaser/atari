dc01ff01ab0f6b7f7df5190041f8fc3d8522c999
AustinR <slidechaser@gmail.com>
2021-05-24 13:41:53 -0700

    finished clean mem and color bg

--- colorbg/colorbg.asm
+++ colorbg/colorbg.asm
@@ -3,7 +3,24 @@
     include "macro.h"
 
     seg code
-    org $ F000      ; Defines origin of the ROM at $F000
+    org $F000      ; Defines origin of the ROM at $F000
 
 START:
     CLEAN_START     ; Macro to safely clear the memory
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+:;  Set Background Luminosity Color to Yellow   ;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    lda #$1E        ; Load color into A ($1E is NTSC yellow)
+    sta COLUBK      ; Store A to BackgroundColor Address $09
+
+    jmp START       ; Repeat from START
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+:;  Fill ROM size to exactly 4KB                ;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    org $FFFC       ; Defines origin to $FFFC
+    .word START     ; Reset vector at $FFFC (where the program starts)
+    .word START     ; Interrupt vector at $FFFE (unused in the VCS)
