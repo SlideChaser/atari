c96fdb4b36c86fee331b3a5c8460d9d1644296ab
Austin Reilly <areilly@alphaworks.tech>
2021-06-19 01:30:38 -0700

    tried a little hex bitmap on the objects project made the falling apple animation in vertical-position. Moving on to horizontal position

--- screen-objects/screen-objects.asm
+++ screen-objects/screen-objects.asm
@@ -5,6 +5,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	include "vcs.h"
 	include "macro.h"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start an uninitialized segment at $80 for variable declaration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    seg.u Variables
+    org $80
+P0Height ds 1   ; defines one byte for player 0 height
+P1Height ds 1   ; defines one byte for player 1 height    
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start our ROM code
@@ -15,11 +23,15 @@
 Reset:
 	CLEAN_START
         
-    ldx #$80	; blue background color
+    ldx #255	; blue background color
     stx COLUBK
     
     lda #$ff	; white playfield color
     sta COLUPF
+
+    lda #10         ; A = 10
+    sta P0Height    ; P0Height = 10
+    sta P1Height    ; P1Height = 10
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Set the TIA registers for the colors of P0 and P1
@@ -29,6 +41,9 @@ Reset:
 
     lda #$C6    ; Player 1 color light-green
     sta COLUP1
+
+    ldy #%00000010
+    sty CTRLPF
         
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Start a new frame by configuring VBLANK and VSYNC
@@ -102,7 +117,7 @@ Player0Loop
     sta GRP0
     sta WSYNC
     iny
-    cpy #10
+    cpy P0Height
     bne Player0Loop
 
     lda #0
@@ -118,7 +133,7 @@ Player1Loop
     sta GRP1
     sta WSYNC
     iny
-    cpy #10
+    cpy P1Height
     bne Player1Loop
 
     lda #0
@@ -154,7 +169,8 @@ Player1Loop
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     org $FFE8
 PlayerBitmap:
-    .byte #%01111110
+    ; .byte #%0111 1110
+    .byte #$7E
     .byte #%11111111
     .byte #%10011001
     .byte #%11111111
